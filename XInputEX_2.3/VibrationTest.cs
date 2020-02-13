ec2f6ff0ca96e09456d7a674b0eff7d2f96e8ad2
Kevin van As <kevin.vanas@student.hku.nl>
2020-02-13 21:29:52 +0100

    v2.3r1
    - Removed VibrationTest.cs

--- XInputEX_2.3/VibrationTest.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using XInputDotNetExtended;
-
-public class VibrationTest : MonoBehaviour
-{
-	public Vibration test;
-}
